6eddcdd8d3f3192170d98ab3dbdebc72a02f3466
vv.kar <v.v.karankevich@gmail.com>
2022-03-13 15:43:18 +0300

    Ch 3 Task 8 completed

---
+++ Study.Projects.Ch3Task8/Ch3Task8.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Study.Projects.Ch3Task8
+{
+    /// <summary>
+    /// Write a program that prints a sequence of Fibonacci numbers.
+    /// </summary>
+    internal class Ch3Task8
+    {
+        public static void Main()
+        {
+            string enterMessage = "Enter a number: ";
+
+            string errorMessage = "Incorrect input!";
+
+            Console.Write(enterMessage);
+
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                int[] numbers = { 0, 1 };
+
+                string resultMessage = $"The sum of {number} first Fibonacci numbers: ";
+
+                for (int i = 0, sumOfTwo; i < number; i++)
+                {
+                    Console.Write($"{numbers[1]} ");
+
+                    sumOfTwo = numbers.Sum();
+
+                    numbers[0] = numbers[1];
+
+                    numbers[1] = sumOfTwo;
+                }
+            }
+            else
+            {
+                Console.WriteLine(errorMessage);
+            }
+        }
+    }
+}
