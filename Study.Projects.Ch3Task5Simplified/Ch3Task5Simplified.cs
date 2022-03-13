aa3ef20a8d106f47fa186c5268c741c4a6a10e0d
vv.kar <v.v.karankevich@gmail.com>
2022-03-13 14:09:10 +0300

    Ch 3 Task 5 simplified

---
+++ Study.Projects.Ch3Task5Simplified/Ch3Task5Simplified.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Study.Projects.Ch3Task5Simplified
+{
+    internal class Ch3Task5Simplified
+    {
+        public enum DaysOfWeekLowerCase
+        {
+            monday = 1,
+            tuesday = 2,
+            wednesday = 3,
+            thursday = 4,
+            friday = 5,
+            saturday = 6,
+            sunday = 7
+        }
+
+        public static void Main()
+        {
+            string enterMessage = "Enter the day of week: ";
+
+            string errorMessage = "Incorrect input";
+
+            string outputMessage = "The number of entered day: ";
+
+            while (true)
+            {
+                Console.Write(enterMessage);
+
+                string input = Console.ReadLine().ToLower();
+
+                if(Enum.TryParse(input, out DaysOfWeekLowerCase day))
+                {
+                    Console.WriteLine(outputMessage + (int)day);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(errorMessage);
+                }
+            }
+        }
+    }
+}
