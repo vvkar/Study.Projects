a678e7419b433aea870ce8c04d0237a5aa2ba971
vv.kar <v.v.karankevich@gmail.com>
2022-03-15 17:20:17 +0300

    Ch 4 Task 5 completed
    Write a program that creates an array and fills it with random numbers. The array is displayed in the console. In this array, you need to determine the element with the minimum value. In particular, the program should output the value of the element with the minimum value and the index of that element. If there are multiple elements, the indexes of all those elements should be printed.

---
+++ Study.Projects.Ch4Tasks/Ch4Task5/InputChecker.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Ch4Task5
+{
+    class InputChecker
+    {
+        /// <summary>
+        /// Displays an input message and accepts the value that the user has entered.
+        /// </summary>
+        /// <returns>A string with users input data.</returns>
+        public static string InputEnter()
+        {
+            string enterMessage = "Enter a number: ";
+
+            Console.Write(enterMessage);
+
+            return Console.ReadLine();
+        }
+        /// <summary>
+        /// Tests a string value for integer and non-negativity.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>Returns an integer from input.</returns>
+        public static int PositiveIntInputCheck(string input)
+        {
+            string errorMessage = "Incorrect input!";
+
+            string negativeInput = "Your input is negative!";
+
+            if (!int.TryParse(input, out int result))
+            {
+                Console.WriteLine(errorMessage);
+
+                result = PositiveIntInputCheck(InputEnter());
+            }
+            else if (result < 0)
+            {
+                Console.WriteLine(negativeInput);
+
+                result = PositiveIntInputCheck(InputEnter());
+            };
+            return result;
+        }
+    }
+}
