0f61dc3568b2c44a2cba11ecef58ca54ac5de9dd
vv.kar <v.v.karankevich@gmail.com>
2022-03-16 19:30:22 +0300

    Ch 5 Task 1 completed
    Write a program that describes a static method for calculating the double factorial of a number passed as an argument to this method. Suggest a variant without recursion and with recursion.

---
+++ Study.Projects.Ch5Tasks/Ch5Task1/Ch5Task1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5Tasks
+{
+    /// <summary>
+    /// Write a program that describes a static method for 
+    /// calculating the double factorial of a number passed 
+    /// as an argument to this method. Suggest a variant 
+    /// without recursion and with recursion.
+    /// </summary>
+    internal class Ch5Task1
+    {
+        static void Main(string[] args)
+        {
+            int num, result;
+
+            do
+            {
+                num = InputChecker.EnterPositive();
+            }
+            while (!DoubleFact(num, out result));
+
+            int recursiveResult = DoubleFactRecursive(num);
+
+            Console.WriteLine(num + "!! = " + result);
+
+            Console.WriteLine(num + "!! = " + recursiveResult + " - recursive");
+        }
+
+        static bool DoubleFact(int num, out int result)
+        {
+            result = 0;
+
+            if (num < 3)
+            {
+                Console.WriteLine("Number should be 3 or more!");
+                return false;
+            }
+            else
+            {
+                result = 1;
+
+                for (int i = num; i > 0; i -= 2)
+                {
+                    result *= i;
+                }
+                return true;
+            }
+        }
+
+        static int DoubleFactRecursive(int num)
+        {
+            if (num < 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return num * DoubleFactRecursive(num - 2);
+            }
+        }
+    }
+}
---
+++ Study.Projects.Ch5Tasks/ClassLibrary/InputChecker.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch5Tasks
+{
+    public class InputChecker
+    {
+        public static int EnterPositive()
+        {
+            string enterMessage = "Enter positive number: ";
+            string errorMessage = "Incorrect input!";
+            string negativeMessage = "Your number should be positive!";
+            int result;
+            string input;
+
+            while (true)
+            {
+                Console.Write(enterMessage);
+
+                input = Console.ReadLine();
+
+                if (!int.TryParse(input, out result))
+                {
+                    Console.WriteLine(errorMessage);
+                }
+                else if (result <= 0)
+                {
+                    Console.WriteLine(negativeMessage);
+                }
+                else break;
+            }
+
+            return result;
+        }
+    }
+}
