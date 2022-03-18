a6c2a4e7ab9ac6001ab974de07abb585ef40943c
vv.kar <v.v.karankevich@gmail.com>
2022-03-18 17:19:33 +0300

    Ch 6 Task 1 completed
    Write a program with a class that has a private character field and three public methods. One method allows you to assign a value to a field. Another method, when called, returns a character code. The third method allows you to display the symbol (field value) and its code in the console window.

---
+++ Study.Projects.Ch6Tasks/Ch6Task1/Ch6Task1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch6Tasks
+{
+    internal class Ch6Task1
+    {
+        /// <summary>
+        /// Write a program with a class that has a private character field 
+        /// and three public methods. One method allows you to assign a value 
+        /// to a field. Another method, when called, returns a character code. 
+        /// The third method allows you to display the symbol (field value) 
+        /// and its code in the console window.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            char symb = InputChecker.InputChar();
+
+            MyClass obj = new MyClass();
+
+            obj.Set(symb);
+
+            int code = obj.GetCode();
+
+            obj.Print();
+
+            Console.WriteLine("Its code: " + code);
+        }
+
+        class MyClass
+        {
+            private char symb;
+
+            public void Set(char symb)
+            {
+                this.symb = symb;
+            }
+
+            public int GetCode()
+            {
+                return this.symb;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine("The symbol is: " + this.symb);
+            }
+        }
+    }
+}
---
+++ Study.Projects.Ch6Tasks/ClassLibrary/InputChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch6Tasks
+{
+    public class InputChecker
+    {
+        public static char InputChar()
+        {
+            string enterMessage = "Enter any symbol: ";
+            string quantityError = "You input sould consist of only one symbol!";
+            string input;
+            do
+            {
+                Console.Write(enterMessage);
+                input = Console.ReadLine();
+
+                if (input.Length > 1 || input.Length == 0)
+                {
+                    Console.WriteLine(quantityError);
+                }
+            }
+            while (input.Length > 1 || input.Length == 0);
+
+            return input[0];
+        }
+    }
+}
