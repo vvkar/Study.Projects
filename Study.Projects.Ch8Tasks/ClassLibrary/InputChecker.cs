d07bbd2b17ae11f9a9c653aadd495c815d04e38e
vv.kar <v.v.karankevich@gmail.com>
2022-03-24 12:48:33 +0300

    Ch8Task1 completed

---
+++ Study.Projects.Ch8Tasks/ClassLibrary/InputChecker.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch8Tasks
+{
+    public class InputChecker
+    {
+        static string errorMessage = "Incorrect input!";
+        public static string EnterString(string message)
+        {
+            string inputLengthError = "Your input is too short!";
+            string input;
+
+            do
+            {
+                Console.Write(message);
+
+
+                input = Console.ReadLine();
+
+                if (input.Length < 1)
+                {
+                    Console.WriteLine(inputLengthError);
+                }
+            }
+            while (input.Length < 1);
+
+            return input;
+        }
+
+        public static string EnterString()
+        {
+            return EnterString("Enter any sentence: ");
+        }
+
+        public static char EnterChar()
+        {
+            string enterMessage = "Enter any symbol: ";
+
+            return EnterChar(enterMessage);
+        }
+
+        public static char EnterChar(string message)
+        {
+            string quantityError = "You input sould consist of only one symbol!";
+            string input;
+            do
+            {
+                Console.Write(message);
+                input = Console.ReadLine();
+
+                if (input.Length > 1 || input.Length == 0)
+                {
+                    Console.WriteLine(quantityError);
+                }
+            }
+            while (input.Length > 1 || input.Length == 0);
+
+            return input[0];
+        }
+
+        public static int EnterInteger()
+        {
+            string enterMessage = "Enter a number: ";
+
+            return EnterInteger(enterMessage);
+        }
+
+        public static int EnterInteger(string message)
+        {
+            int result;
+            string input;
+
+            while (true)
+            {
+                Console.Write(message);
+
+                input = Console.ReadLine();
+
+                if (!int.TryParse(input, out result))
+                {
+                    Console.WriteLine(errorMessage);
+                }
+                else break;
+            }
+            return result;
+        }
+    }
+}
