4cff4253dce988501bc7797fb6854144ee25d1e7
vv.kar <v.v.karankevich@gmail.com>
2022-03-31 20:14:27 +0300

    Ch1Task5 completed

---
+++ Part 2/Part2.Ch1Tasks/Ch1Task5/Ch1Task5.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch1Tasks
+{
+    internal class Ch1Task5
+    {
+        static void Main(string[] args)
+        {
+            TaskClass getter = new TaskClass();
+            InputChecker checker = new InputChecker();
+
+            int number = checker.EnterNonNegativeInteger();
+            Console.WriteLine($"Char by your code {number} is {getter.GetCharByCode(number)}");
+
+            char symbol = checker.EnterChar();
+            Console.WriteLine($"Code of your symbol {symbol} is {getter.GetCharCode(symbol)}");
+        }
+
+        class TaskClass : ICharable, IIntable
+        {
+            public int GetCharCode(char symbol)
+            {
+                return symbol;
+            }
+
+            public char GetCharByCode(int code)
+            {
+                return (char)code;
+            }
+        }
+
+        interface ICharable
+        {
+            int GetCharCode(char symbol);
+        }
+
+        interface IIntable
+        {
+            char GetCharByCode(int code);
+        }
+    }
+}
