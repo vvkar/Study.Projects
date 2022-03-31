f94f5b773d2b29567c4252778466a8f78c544c8f
vv.kar <v.v.karankevich@gmail.com>
2022-03-31 20:20:47 +0300

    Ch1Task6 completed

---
+++ Part 2/Part2.Ch1Tasks/Ch1Task6/Ch1Task6.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ch1Tasks
+{
+    internal class Ch1Task6
+    {
+        static void Main(string[] args)
+        {
+            TaskClass indexerObj = new TaskClass();
+            InputChecker checker = new InputChecker();
+
+            int number = checker.EnterNonNegativeInteger();
+            Console.WriteLine($"indexerObj[{number}] = {indexerObj[number]}");
+
+            char symbol = checker.EnterChar();
+            Console.WriteLine($"indexerObj[{symbol}] = {indexerObj[symbol]}");
+        }
+
+        class TaskClass : IIndexerByChar, IIndexerByInt
+        {
+            public int this[char symbol] => symbol;
+
+            public char this[int number] => (char)number;
+        }
+
+        interface IIndexerByChar
+        {
+            int this[char symbol] { get; }
+        }
+
+        interface IIndexerByInt
+        {
+            char this[int number] { get; }
+        }
+    }
+}
