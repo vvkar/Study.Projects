c8fc6f24f941f6fbb66262e6fb67779acc19be9a
vv.kar <v.v.karankevich@gmail.com>
2022-04-03 19:35:56 +0300

    Ch2Task6 completed
    Write a program that uses delegates and anonymous methods (lambda expressions) to create an instance of a delegate that is called without arguments and returns a text value with the name of the day of the week ("Monday", "Tuesday" and so on until "Sunday"). Each time the instance is called again, the name of the next day of the week is returned as a result. After "Sunday" the result returns "Monday" and so on.

---
+++ Part 2/Part2.Ch2Tasks/Ch2Task6/Ch2Task6.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Ch2Tasks
+{
+    /// <summary>
+    /// Write a program that uses delegates and anonymous 
+    /// methods (lambda expressions) to create an instance 
+    /// of a delegate that is called without arguments and 
+    /// returns a text value with the name of the day of 
+    /// the week ("Monday", "Tuesday" and so on until "Sunday"). 
+    /// Each time the instance is called again, the name of the 
+    /// next day of the week is returned as a result. 
+    /// After "Sunday" the result returns "Monday" and so on.
+    /// </summary>
+    internal class Ch2Task6
+    {
+        static void Main(string[] args)
+        {
+            DayOfWeek dayOfWeek = DayOfWeek.Sunday;
+
+            WeekdayHandler weekDayShow = () =>
+            {
+                if ((int)dayOfWeek == 6)
+                {
+                    dayOfWeek = DayOfWeek.Sunday;
+                }
+                else dayOfWeek++;
+                return dayOfWeek.ToString();
+            };
+
+            do
+            {
+                Console.WriteLine(weekDayShow());
+                Console.ReadKey();
+            }
+            while (true);
+        }
+
+        delegate string WeekdayHandler();
+    }
+}
