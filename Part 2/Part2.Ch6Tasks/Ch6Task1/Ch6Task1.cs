fbfc56928fd266dea935c4ba789d8153229e704a
vv.kar <v.v.karankevich@gmail.com>
2022-04-05 19:02:31 +0300

    Ch6Task1 completed
    Write a program that computes factorial, double factorial, and a Fibonacci number for a given integer value. A child thread is started to calculate each of the values. Factorial n! for the number n is calculated as the product of numbers from 1 to this number inclusive, that is, n! = 1 × 2 × 3 × ... × n. Double factorial n!! is calculated as the product of natural numbers "through one", that is, n!! = n × (n − 2) × (n − 4) × ... (the last factor is 2 or 1, depending on whether n is even or odd). In the Fibonacci sequence, the first two numbers are 1, and each next number is equal to the sum of the previous two (the numbers are 1, 1, 2, 3, 5, 8, 13, 21, and so on).

---
+++ Part 2/Part2.Ch6Tasks/Ch6Task1/Ch6Task1.cs
@@ -0,0 +1,69 @@
+﻿using ClassLibrary;
+using System.Threading;
+
+class Ch6Task1
+{
+    static void Main()
+    {
+        InputChecker inputChecker = new InputChecker();
+        int number = inputChecker.EnterNonNegativeInteger();
+        int threadSleep = 500;
+
+        Thread threadOne = new(() =>
+        {
+            Console.WriteLine("Start factorial calculating!");
+            Thread.Sleep(threadSleep);
+            Console.WriteLine($"{number}! = {GetFact(number)}");
+        });
+
+        Thread threadTwo = new(() =>
+        {
+            Console.WriteLine("Start double factorial calculating!");
+            Thread.Sleep(threadSleep);
+            Console.WriteLine($"{number}!! = {GetDoubleFact(number)}");
+        });
+
+        Thread threadThree = new(() =>
+        {
+            Console.WriteLine("Start Fibonacci calculating!");
+            Thread.Sleep(threadSleep);
+            Console.WriteLine($"Fibonacci #{number} = {GetFibonacci(number)}");
+        });
+
+        threadOne.Start();
+        threadTwo.Start();
+        threadThree.Start();
+
+    }
+
+    static int GetFact (int number)
+    {
+        int result = 1;
+        for(int i = 1; i <= number; i++)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    static int GetDoubleFact(int number)
+    {
+        int result = 1;
+        for(int i = number; i > 0; i -= 2)
+        {
+            result *= i;
+        }
+        return result;
+    }
+
+    static int GetFibonacci(int number)
+    {
+        int first = 0, second = 1;
+        for (int i = 2; i < number; i++)
+        {
+            second += first;
+            first = second - first;
+        }
+        return second;
+    }
+}
---
+++ Part 2/Part2.Ch6Tasks/ClassLibrary/InputChecker.cs
@@ -0,0 +1,116 @@
+﻿namespace ClassLibrary
+{
+    public class InputChecker
+    {
+        static string errorMessage = "Incorrect input!";
+        public string EnterString(string message)
+        {
+            string inputLengthError = "Your input is too short!";
+            string input;
+
+            do
+            {
+                Console.Write(message);
+
+
+                input = Console.ReadLine();
+
+                if (input.Length < 1)
+                {
+                    Console.WriteLine(inputLengthError);
+                }
+            }
+            while (input.Length < 1);
+
+            return input;
+        }
+
+        public string EnterString()
+        {
+            return EnterString("Enter any sentence: ");
+        }
+
+        public char EnterChar()
+        {
+            string enterMessage = "Enter any symbol: ";
+
+            return EnterChar(enterMessage);
+        }
+
+        public char EnterChar(string message)
+        {
+            string quantityError = "You input sould consist of only one symbol!";
+            string input;
+            do
+            {
+                Console.Write(message);
+                input = Console.ReadLine();
+
+                if (input.Length > 1 || input.Length == 0)
+                {
+                    Console.WriteLine(quantityError);
+                }
+            }
+            while (input.Length > 1 || input.Length == 0);
+
+            return input[0];
+        }
+
+        public int EnterInteger()
+        {
+            string enterMessage = "Enter a number: ";
+
+            return EnterInteger(enterMessage);
+        }
+
+        public int EnterIntegerMoreThenZero(string enterMessage)
+        {
+            int result;
+
+            do
+            {
+                result = EnterInteger(enterMessage);
+            }
+            while (result <= 0);
+
+            return result;
+        }
+
+        public int EnterIntegerMoreThenZero()
+            => EnterIntegerMoreThenZero(enterMessage: "Enter a number more then zero: ");
+
+        public int EnterInteger(string message)
+        {
+            int result;
+            string input;
+
+            while (true)
+            {
+                Console.Write(message);
+
+                input = Console.ReadLine();
+
+                if (!int.TryParse(input, out result))
+                {
+                    Console.WriteLine(errorMessage);
+                }
+                else break;
+            }
+            return result;
+        }
+
+        public int EnterNonNegativeInteger(string enterMessage)
+        {
+            int result;
+            do
+            {
+                result = EnterInteger(enterMessage);
+            }
+            while (result <= 0);
+            return result;
+        }
+
+        public int EnterNonNegativeInteger() =>
+            EnterNonNegativeInteger(enterMessage: "Enter not negative integer: ");
+    }
+}
